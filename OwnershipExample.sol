fa230ef15217a0d61636728fec52de9f0a5a4d8c
Shreya Marakana <shreyamarakana21@gmail.com>
2026-06-06 13:04:19 +0530

    Update OwnershipExample.sol

--- OwnershipExample.sol
+++ OwnershipExample.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract OwnershipExample {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+}
