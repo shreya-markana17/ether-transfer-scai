098702233c618d2b44cd411d30ec5cda84e9a373
Shreya Marakana <shreyamarakana21@gmail.com>
2026-06-15 22:16:27 +0530

    Create AchievementBadgeNFT.sol

---
+++ AchievementBadgeNFT.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract AchievementBadgeNFT {
+
+    string public name = "Achievement Badge NFT";
+    string public symbol = "ABNFT";
+
+    uint256 public totalSupply;
+
+    mapping(uint256 => address) public ownerOf;
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    constructor() {
+        totalSupply = 0;
+    }
+
+    function mint(address to) public {
+        totalSupply++;
+        ownerOf[totalSupply] = to;
+        balanceOf[to]++;
+
+        emit Transfer(address(0), to, totalSupply);
+    }
+}
